8289c1385e3503d47e8895ab0ba85ed439ecdaae
Banujan Ravishankar <banujanr29@gmail.com>
2026-06-26 21:48:10 +0530

    chore: remove legacy Vite frontend

--- frontend_old/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Components barrel file.
- *
- * Shared/reusable components will be exported from here.
- * Navbar is already in this directory.
- */
-export { default as Navbar } from './Navbar'
--- frontend_old/src/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Custom hooks barrel file.
- *
- * Domain-specific hooks (e.g. useAudit, useHealth) will be exported
- * from here once business logic is implemented.
- */
-
-// TODO: export domain hooks when implemented
-export {}
